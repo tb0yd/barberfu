e40e54f8b98a822983f67f2a2edf34830d79dfbc
Tyler Boyd <tboyd47@gmail.com>
2011-01-11 09:58:44 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barberfu::Application.config.secret_token = '51978a61dbff799caa05585bb431894d375135b61df51db143d8c98f642b462b6dad3a24a597708673a6fe03993c843a86817703fd2d65a019781e2e9549277e'
