b7fed2d066bfd243ce00069c5697552b3c42f806
Tyler Boyd <tboyd47@gmail.com>
2011-01-21 15:10:35 -0500

    start TypoCheck

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,17 @@
 class Review < ActiveRecord::Base
+  def typo_check
+    
+  end
+end
+
+class TypoCheck
+  attr_accessor :word
+  cattr_accessor :dictionary
+  
+  def initialize(word)
+    @word = word
+  end
+  def result
+    !@@dictionary.include?(@word)
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,3 +3,17 @@ require 'spec_helper'
 describe Review do
   
 end
+
+describe TypoCheck do
+  before { TypoCheck.dictionary = ["one", "two", "three"] }
+  
+  context "no typo" do
+    subject { TypoCheck.new("one").result }
+    it {should be_false}
+  end
+  
+  context "yes typo" do
+    subject { TypoCheck.new("eno").result }
+    it {should be_true}
+  end
+end
